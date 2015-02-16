2e748cb382111acb06a1edcbaf1a62185ba9585c
Adam Londero <alondero@gmail.com>
2015-02-16 00:16:55 +0000

    Changed Formula1.py to be more simple

--- Formula1.py
+++ Formula1.py
@@ -1,195 +1,49 @@
 #!/usr/bin/env python
 
-#     Copyright (C) 2013  Casey Duquette
-# 
-#     This program is free software; you can redistribute it and/or modify
-#     it under the terms of the GNU General Public License as published by
-#     the Free Software Foundation; either version 2 of the License, or
-#     (at your option) any later version.
-# 
-#     This program is distributed in the hope that it will be useful,
-#     but WITHOUT ANY WARRANTY; without even the implied warranty of
-#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#     GNU General Public License for more details.
-
-"""
-Custom scanner plugin for Plex Media Server for Formula1 Brodcast.
-"""
-
-import re, os, os.path
 import sys
+import os
 import logging
 
-# I needed some plex libraries, you may need to adjust your plex install location accordingly
-sys.path.append("/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Scanners/Series")
-sys.path.append("/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-ins/Scanners.bundle/Contents/Resources/Common/")
-import Media, VideoFiles, Stack, Utils
-from mp4file import mp4file, atomsearch
+#Modify these paths to be the location of Plex
+sys.path.append("...../plex/Library/Application Support/Plex Media Server/Scanners/Series")
+sys.path.append("...../plex/Library/Application Support/Plex Media Server/Plug-ins/Scanners.bundle/Contents/Resources/Common/")
 
-__author__ = "Kenneth Ellefsen"
-__copyright__ = "Copyright 2014"
-__credits__ = ["Kenneth Ellefsen"]
+import re
+import Media
+import VideoFiles
+import Stack
+import Utils
 
-__license__ = "GPLv2"
-__version__ = "1.0"
-__maintainer__ = "Kenneth Ellefsen"
-__email__ = ""
+sessionmap = {
+	1: "Qualifying",
+	2: "Race"
+}
 
+episode_regexp = '(?P<year>.*?)\s-\ss(?P<race>[0-9]{2})e(?P<session>0[1-2])\s-\s(?P<episode>[^\.]*)'
 
-locations = { 2014 : 
-                {
-                'australia' : 1,
-                 'malaysia' : 2,
-                 'bahrain' : 3,
-                 'china' : 4,
-                 'chinese' : 4,
-                 'spain' : 5,
-                 'spanish' : 5,
-                 'monaco' : 6,
-                 'canadian' : 7,
-                 'canada' : 7,
-                 'austrian' : 8,
-                 'austria' : 8,
-                 'british' : 9,
-                 'great britain' : 9,
-                 'german' : 10,
-                 'germany' : 10,
-                 'hungarian' : 11,
-                 'hungary' : 11,
-                 'belgian' : 12,
-                 'belgium' : 12,
-                 'italian' : 13,
-                 'italyn' : 13,
-                 'singapore' : 14,
-                 'japanese' : 15,
-                 'japan' : 15,
-                 'russian' : 16,
-                 'russia' : 16,
-                 'united states' : 17,
-                 'us' : 17,
-                 'brazilian' : 18,
-                 'brazil' : 18,
-                 'abu dhabi' : 19,
-                 'unknown' : 99,
-                 } ,
-             }
-
-locationsep = { 2014 :
-                {
-                1 : 0,
-                2 : 0,
-                3 : 0,
-                4 : 0,
-                5 : 0,
-                6 : 0,
-                7 : 0,
-                8 : 0,
-                9 : 0,
-                10 : 0,
-                11 : 0,
-                12 : 0,
-                13 : 0,
-                14 : 0,
-                15 : 0,
-                16 : 0,
-                17 : 0,
-                18 : 0,
-                19 : 0,
-                99 : 0,
-                },
-              }
-
-
-#episode_regexp = '(?P<show>formula1|f1?)[\._ ](?P<season>[0-9]{4})[\._ ](?P<location>.*?)[\.](?P<ep>.*?)\.(720p|HDTV|x254|1080p)'
-episode_regexp = '(?P<show>.*?)[\._ ](?P<season>[0-9]{4})[\._ ](?P<location>.*?)[\.](?P<ep>.*?)\.(720p|HDTV|x254|1080p)'
-
-
-# Look for episodes.
 def Scan(path, files, mediaList, subdirs, language=None, root=None):
-    # Scan for video files.
-    VideoFiles.Scan(path, files, mediaList, subdirs, root)
-  
-    logging.basicConfig(filename='/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Logs/Formula1.log',level=logging.DEBUG)
-    #comment the line below to enable debugging
-    #logging.disable(logging.DEBUG)
-
-    logging.debug("path:"+path)
-
-    # Run the select regexp for all media files.
-    for i in files:
-        logging.debug("file:"+i)
+	VideoFiles.Scan(path, files, mediaList, subdirs, root)
+	
+	for i in files:
+		logging.debug("file:"+i)
         file = os.path.basename(i)
         logging.debug("basename:"+file)
         match = re.search(episode_regexp, file, re.IGNORECASE)
         if match:
-            # Extract data.
-            location = match.group('location').lower().replace("."," ")         # Location typically Spain/Spanish/Italy ...
-            origlocation = location
-            show = 'formula1' if match.group('show').lower() == 'f1' else match.group('show').strip()
-            year = match.group('season').strip()
-            yearint = int(year)
-            logging.debug("year:%s",year)
-            show = show + " " + year # Make a composite show name like Formula1 + yyyy
-            logging.debug("looking for location:%s in locations[%d]",location,yearint)
-            if location not in locations[yearint].viewkeys():
-                location = "unknown"
-                logging.debug("location not found, mapping %s to unknown",location)
-#                continue
-
-            locationidx = locations[yearint][location] #location index is mapped to season
-            locationsep[yearint][locationidx] += 1 # count up ep number for each matching file for a given location
-
-            # episode is just a meaningless index to get the different FP1-3, Qualifying, Race and other files to
-            # be listed under a location i.e. Spain, which again is mapped to season number - as season can not contain a string
-            episode = locationsep[yearint][locationidx] 
-
-            # description will be the displayed filename when you browse to a location (season number)
-
-            logging.debug("show:"+show)
-            logging.debug("location:"+location)
-            logging.debug("episode%d:",episode)
-            description = "";
-
-            if location == "unknown": # reset the location back to get full description
-                location = origlocation
-                logging.debug("resetting location to:%s",origlocation)
-
-            description = location + " " + match.group('ep').replace("."," ") # i.e. # spain grand prix free practice 3
-            logging.debug("description:"+description)
-
-            tv_show = Media.Episode(
-                show,               # show (inc year(season))
-                locationidx,        # locationidx as season (string not supported) 
-                episode,            # episode, indexed the files for a given show/location
-                description,        # includes location string and ep name i.e. Spain Grand Prix Qualifying
-                year)               # the actual year detected, same as used in part of the show name
-
-            logging.debug("tv_show created")
-            tv_show.parts.append(i)
-            logging.debug("part added to tv_shows")
-            mediaList.append(tv_show)
-            logging.debug("added tv_show to mediaList")
-        else:
-            logging.debug("no match")
-
-  
-    # Stack the results.
-    Stack.Scan(path, files, mediaList, subdirs)
-
-def find_data(atom, name):
-  child = atomsearch.find_path(atom, name)
-  data_atom = child.find('data')
-  if data_atom and 'data' in data_atom.attrs:
-    return data_atom.attrs['data']
-
-import sys
-    
-if __name__ == '__main__':
-  print "Hello, world!"
-  path = sys.argv[1]
-  files = [os.path.join(path, file) for file in os.listdir(path)]
-  media = []
-  Scan(path[1:], files, media, [])
-  print "Media:", media
-
+			year = match.group('year')
+			race = int(match.group('race'))
+			sessionnumber = int(match.group('session'))
+			session = sessionmap[sessionnumber]
+			episode = match.group('episode')
+			
+			logging.debug("Found F1 episode - Year: %d, Race: %d, Session: %s, Episode: %s", year, race, session, episode)
+			
+			racetitle = year + " " + str(episode) + " Grand Prix"
+			description = session + " - " + racetitle
+			
+			tv_show = Media.Episode(year, race, sessionnumber, description, year)	
+			tv_show.parts.append(i)
+			mediaList.append(tv_show)
+			logging.debug("tv_show created")
+	Stack.Scan(path, files, mediaList, subdirs)
 
--- README.md
+++ README.md
@@ -1,41 +1,23 @@
-PlexF1MediaScanner
+Simple PlexF1MediaScanner
 ==================
 
-The way that Formula1 boadcasts is organized doesn't fit very well with ether Movies or TV Series. Adding them as home movies is one unsatisfactory option, but instead I tried to make a Formula1 Media Scanner.
- 
-It is working for my purpose, and I take no responsibility should it cause you any harm, but you are free to reuse and modify it.
- 
-How it works:
- 
-I typically have all my Formula1 media in one folder, and one broadcast can be named i.e. formula1.2014.spain.grand.prix.qualifying.uncut.720p.hdtv.x264.mkv. The Formula1 Media Scanner picks up the various parts of the file name using this regular expression:
-'(?P<show>.*?)[\._ ](?P<season>[0-9]{4})[\._ ](?P<location>.*?)[\.](?P<ep>.*?)\.(720p|HDTV|x254|1080p)'
- 
-In the above example it will find the following match:
-MATCH 1
-show [1-9] `formula1`
-season [10-14] `2014`
-location [15-20] `spain`
-ep [21-48] `grand.prix.qualifying.uncut`
-5. [49-53] `720p`
- 
- 
-The challenge here is to map this into a TV Series format. Basically a Formula1 episode is really a full weekend, containing many broadcasts, from buildup, FP1-3, Qualifying, race and others. However, one episode may only contain one media (disregarding stacking, as that is really sub optimal here).
-What I did instead was to assume that this library will only contain Formula1, and I named the show like Formula1 yyyy. So for the example above, the show would resolv to Formula1 2014.
-A series is then mapped to a location, but since series unfortunately only supports integer, I made a look up table to resolve location to Season number (This will have to be updated for years to come, only 2014 added).
-Under a given season (location) I add all the media pertaining to this location (weekend) as episodes.
-If it encounters a locations it doesn't recognize, it will still add the episode, but under season 99 (unknown)
- 
-Thinking out loud
-Ideally I could add a show as Formula1, add seasons by year, add Episodes as location. Opening one Episode allows you to play any file from that weekend, but I don't see how that can be done.
-This scanner should work well for others (motor)sports that are organized in a similar way, you have to update locations though.
-The locations could be added dynamically, but I have added them manually so I get the "Seasons" in the correct order of the calender.
- 
+Forked from PlexF1MediaScanner.
+
+A more simple version of the PlexF1MediaScanner that can handle any season (not just 2014). Read that readme for more details about why the project came about.
+
+Currently this relies upon a file structure of <YEAR>/<YEAR> - s<RACE_NUMBER>e<QUALIFYING OR RACE?1:2> - <COUNTRY>.
+
+For example 1998/1998 - s01e01 - Australia, which represents the Qualifying session for the 1998 Australian GP. 
+
+WIP - Metadata Agent to go with this, grabbing a better title and description from Wikipedia? 
  
 Install and use:
- 
-copy the attached file to /var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Scanners/Series/
+
+clone repository
+modify Formula1.py to correct the location of your plex directory
+copy the attached file to <PLEX DIRECTORY>/Library/Application Support/Plex Media Server/Scanners/Series/
 restart plexmediaserver
-Add a new library called Formula1 and add the folder containing F1 broadcasts
+Add a new Television library and add the folder containing F1 broadcasts
 Select Advanced and Scanner: Formula1
  
 Bugs:
